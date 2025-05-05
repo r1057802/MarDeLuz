4f8a71db2b10785096dcef1aea85dd3359d57ccc
diazv <diazv@GUSTAVO>
2025-05-05 14:09:28 +0200

    eerste master update

---
+++ test1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarDeLuz
+{
+    internal class test1
+    {
+        public int TEST { get; set; }
+    }
+}
